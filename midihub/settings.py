4ac90ea1fabc41c56c31607494a051e61b9aa6a3
Danil Khismatulin <nexcod@gmail.com>
2019-04-07 21:17:24 +0500

    New styles

--- midihub/settings.py
+++ midihub/settings.py
@@ -108,7 +108,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "node_modules/spectre.css/dist"),
+    os.path.join(BASE_DIR, "frontend/build"),
 ]
 
 
